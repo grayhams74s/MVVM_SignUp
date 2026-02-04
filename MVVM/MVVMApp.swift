07e0477d2c710f9b83b0c491442a17da25afdc12
grayhams74s <felixjunang@gmail.com>
2026-02-04 18:15:37 +0800

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by felix angcot jr on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
